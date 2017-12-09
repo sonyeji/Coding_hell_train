d054204a3e6aaf3b9179f0546d7b532c988e9953
cragy0516 <cragy0516@gmail.com>
2017-12-09 12:37:33 +0900

    first commmiiiit

---
+++ hellcoding/src/termProject/MainClass.java
@@ -0,0 +1,11 @@
+package termProject;
+
+public class MainClass {
+	public static void main (String args[]) {
+		System.out.println("test");
+		Phone pn1 = new Phone("01012345678");
+		Phone pn2 = new Phone("010-7476-4727");
+		System.out.println(pn1);
+		System.out.println(pn2);
+	}
+}
